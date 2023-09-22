cc9301f36902ad44497e7273720a4ee24e4d02ad
Guilherme Paterno <guipaterno@gmail.com>
2023-09-22 17:01:50 -0300

    finalização front end
    finalização front end

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,9 @@
 import { useNavigation } from '@react-navigation/native';
 import { VStack, Image, Text, Center, Heading, ScrollView} from 'native-base';
+import {useForm, Controller} from "react-hook-form";
+import * as yup from "yup";
+import {yupResolver} from "@hookform/resolvers/yup";
+
 
 import { AuthNavigatorRoutesProps } from '@routes/auth.routes';
 
@@ -9,14 +13,31 @@ import BackgroundImg from "@assets/background.png"
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
+type Props ={
+    email:string;
+    password:string;
+}
 
+const signInSchema = yup.object({
+    email: yup.string().required("Informe o e-email.").email("E-mail inválido."),
+    password: yup.string().required("Informe a senha.").min(6, "A senha deve ter pelo menos 6 dígitos."),
+});
 
 export function SignIn(){
 
-    const navigation = useNavigation <AuthNavigatorRoutesProps>();
+    const {control, handleSubmit, formState:{errors}} = useForm<Props>({
+        resolver: yupResolver(signInSchema)
+    });
+
+    const navigation = useNavigation<AuthNavigatorRoutesProps>();      
 
     function handleNewAccount (){
         navigation.navigate("signUp");
+    }
+
+    function handleSignIn({email, password}:Props){
+        console.log({email, password})
+       
     }
 
 
@@ -42,18 +63,41 @@ export function SignIn(){
             Acesse sua conta
            </Heading>
 
-           <Input
+           <Controller
+            control={control}
+            name="email"
+            
+            render={({field:{onChange, value}})=>(
+                <Input
                 placeholder='E-mail'
                 keyboardType='email-address'
                 autoCapitalize='none'
-            />
-           <Input 
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.email?.message}
+                 />
+            )}
+        />
+        
+
+        <Controller
+            control={control}
+            name="password"
+            render={({field:{onChange, value}})=>(
+                <Input 
                 placeholder='Senha'
                 secureTextEntry
-           />
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.password?.message}
+
+                 />
+            )}
+        /> 
 
            <Button
            title='Acessar'
+           onPress={handleSubmit(handleSignIn)}
            />
 
            
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,13 +1,15 @@
 import { useNavigation } from '@react-navigation/native';
-
 import { VStack, Image, Text, Center, Heading, ScrollView} from 'native-base';
+import {useForm, Controller} from "react-hook-form";
+import * as yup from "yup";
+import {yupResolver} from "@hookform/resolvers/yup"
 
-import LogoSvg from "@assets/logo.svg"
-import BackgroundImg from "@assets/background.png"
+import LogoSvg from "@assets/logo.svg";
+import BackgroundImg from "@assets/background.png";
+
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
-import {useForm, Controller} from "react-hook-form"
 
 type FormDataProps ={
     name:string;
@@ -16,9 +18,18 @@ type FormDataProps ={
     password_confirm:string;
 }
 
+const signUpSchema = yup.object({
+    name: yup.string().required("Informe o nome."),
+    email: yup.string().required("Informe o e-email.").email("E-mail inválido."),
+    password: yup.string().required("Informe a senha.").min(6, "A senha deve ter pelo menos 6 dígitos."),
+    password_confirm: yup.string().required("Confirme a senha.").oneOf([yup.ref("password"), null], "A confirmação da senha não confere.")
+});
+
 export function SignUp(){
    
-    const {control, handleSubmit} = useForm<FormDataProps>();
+    const {control, handleSubmit, formState:{errors}} = useForm<FormDataProps>({
+        resolver: yupResolver(signUpSchema)
+    });
 
 
     const navigation = useNavigation();
@@ -31,8 +42,6 @@ export function SignUp(){
         console.log({name, email, password, password_confirm})
        
     }
-
-
 
     return(
         <ScrollView contentContainerStyle={{flexGrow:1}} showsVerticalScrollIndicator={false}>
@@ -59,11 +68,13 @@ export function SignUp(){
         <Controller
             control={control}
             name="name"
+
             render={({field:{onChange, value}})=>(
                 <Input
                 placeholder='Nome'
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.name?.message}
                  />
             )}
         />
@@ -71,6 +82,7 @@ export function SignUp(){
         <Controller
             control={control}
             name="email"
+            
             render={({field:{onChange, value}})=>(
                 <Input
                 placeholder='E-mail'
@@ -78,9 +90,11 @@ export function SignUp(){
                 autoCapitalize='none'
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.email?.message}
                  />
             )}
         />
+        
 
         <Controller
             control={control}
@@ -91,6 +105,8 @@ export function SignUp(){
                 secureTextEntry
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.password?.message}
+
                  />
             )}
         /> 
@@ -104,6 +120,7 @@ export function SignUp(){
                 secureTextEntry
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.password_confirm?.message}
                 onSubmitEditing={handleSubmit(handleSignUp)}
                 returnKeyType="send"
                  />
@@ -111,7 +128,7 @@ export function SignUp(){
         />
          
            <Button
-           title='Criar e acessar'
+           title='Criar e acessar'  
            onPress={handleSubmit(handleSignUp)}
            />
 
@@ -122,7 +139,7 @@ export function SignUp(){
            <Button
            title='Voltar para o login'
            variant="outline"
-           mt={24}
+           mt={12}
            onPress={handleGoBack}
            />
      
