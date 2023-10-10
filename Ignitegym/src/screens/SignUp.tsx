6180f69a30e1dd1400a63c9c6970c8fbe88d4ffe
Guilherme Paterno <guipaterno@gmail.com>
2023-10-10 11:49:44 -0300

    att
    att

--- Ignitegym/src/screens/SignUp.tsx
+++ Ignitegym/src/screens/SignUp.tsx
@@ -1,14 +1,21 @@
 import { useNavigation } from '@react-navigation/native';
-import { VStack, Image, Text, Center, Heading, ScrollView} from 'native-base';
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast} from 'native-base';
 import {useForm, Controller} from "react-hook-form";
 import * as yup from "yup";
 import {yupResolver} from "@hookform/resolvers/yup"
 
+
+import axios from "axios"
+import { api } from '@services/api';
+
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 
+import { AppError } from '@utils/AppError';
+
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import { Alert } from 'react-native';
 
 
 type FormDataProps ={
@@ -26,6 +33,8 @@ const signUpSchema = yup.object({
 });
 
 export function SignUp(){
+
+    const toast = useToast();
    
     const {control, handleSubmit, formState:{errors}} = useForm<FormDataProps>({
         resolver: yupResolver(signUpSchema)
@@ -38,9 +47,21 @@ export function SignUp(){
         navigation.goBack();
     }
 
-    function handleSignUp({name, email, password, password_confirm}:FormDataProps){
-        console.log({name, email, password, password_confirm})
-       
+    async function handleSignUp({name, email, password}:FormDataProps){
+        try{
+            const response = await api.post("/users", {name, email, password});
+            console.log(response.data);
+        }catch(error){
+           const isAppError = error instanceof AppError;
+           const title = isAppError ? error.message :"não foi possível criar a conta. Tente novamente mais tarde."
+
+           toast.show({
+           title,
+           placement:"top",
+           bgColor:"red.500", 
+           })
+        }
+      
     }
 
     return(
